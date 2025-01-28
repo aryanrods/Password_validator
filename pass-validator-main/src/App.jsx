87ab30f58654793313a173864566952c0da718e8
aryanrods <rodriguesaryan00@gmai.com>
2025-01-28 23:43:06 +0530

    done

--- pass-validator-main/src/App.jsx
+++ pass-validator-main/src/App.jsx
@@ -1,7 +1,46 @@
 import "./App.css";
+import React from "react";
+import { useState } from "react";
+import validator from "validator";
 
-function App() {
-  return <></>;
+export default function App() {
+  const [message, setMessage] = useState("");
+
+  const passvalidator = (password) => {
+    if (
+      validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+    ) {
+      setMessage("Password is strong");
+    } else if (
+      validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 0,
+        minSymbols: 0,
+      })
+    ) {
+      setMessage("Password is okay");
+    } else {
+      setMessage("Password is weak");
+    }
+  };
+
+  return (
+    <>
+      <h1>Password Validate</h1>
+      <input
+        type="text"
+        onChange={(e) => passvalidator(e.target.value)}
+        className=""
+      />
+      <p>{message}</p>
+    </>
+  );
 }
-
-export default App;
